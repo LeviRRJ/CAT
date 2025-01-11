2a5afa0d5c4178269a109e8f5bea3d954b882c7b
LeviRRJ <levirileyj@gmail.com>
2025-01-11 13:31:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Responsive Navigation
+const Billy = document.querySelector('.Billy');
+const Chicago = document.querySelector('.Chicago');
+
+Billy.addEventListener('click', () => {
+    Chicago.classList.toggle('open');
+});
